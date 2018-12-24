e052edc236d5759c0c55a41ff3d791d22fcdac85
Michał Dawidowicz <mdawidowicz@efigence.com>
2018-12-24 13:20:15 +0100

    PROJECT SETUP: all dependencies added, sass loader works

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 
-const Index = () => {
-    return <div>hello friend</div>
-}
-
-ReactDOM.render(<Index />, document.getElementById('index'))
+ReactDOM.render(<App />, document.getElementById('index'))
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: './index.html'
+})
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: '[name]_[local]_[hash:base64:5]',
+                            sourceMap: process.env.NODE_ENV !== 'production'
+                        }
+                    },
+                ]
+            }
+        ]
+    },
+    plugins: [htmlPlugin]
+}
