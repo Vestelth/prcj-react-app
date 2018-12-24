17403c117cc7f4cb4d6a9b87d2020c4af847b8c5
Michał Dawidowicz <mdawidowicz@efigence.com>
2018-12-24 12:19:26 +0100

    PROJECT SETUP: react, html, js config finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello friend')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Index = () => {
+    return <div>hello friend</div>
+}
+
+ReactDOM.render(<Index />, document.getElementById('index'))
